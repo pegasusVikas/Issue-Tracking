191b4d84929dbe97bdbbe3463066430e5c07f349
pegasusVikas <vikas.vik934@gmail.com>
2021-03-29 15:14:57 +0530

    signUp Basic check'

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent
+    SignUpComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/src/app/sign-up/sign-up.component.ts
+++ angular/src/app/sign-up/sign-up.component.ts
@@ -6,11 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  errMsg:String="Hello"
+  hasErr:Boolean=true
   constructor() { }
 
   ngOnInit(): void {
   }
+  onSubmit(email:String,username:String,phone:String,password1:String,password2:String){
+    if(password1!=password2)
+    this.showError("Passwords dont match");
+    else if(email=="")
+    this.showError("Email cant be empty");
+    else if(username=="")
+    this.showError("User Name cant be empty");
+    else if(phone=="")
+    this.showError("Mobile Number cant be empty");
+    else if(password1=="")
+    this.showError("password cant be empty");
 
+    //send form to backend
+
+  }
+  showError(error:String){
+    this.errMsg=error
+    this.hasErr=true
+    setTimeout(()=>{this.hasErr=false},2000)
+  }
   log(x: any)
   {
     console.log(x);
