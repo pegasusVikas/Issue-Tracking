44df1842d0f00923bb231f1554dd802a62e8750b
pegasusVikas <vikas.vik934@gmail.com>
2021-03-30 09:18:47 +0000

    CORS all

--- spring/src/main/java/com/example/controller/LoginController.java
+++ spring/src/main/java/com/example/controller/LoginController.java
@@ -4,6 +4,7 @@ package com.example.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,14 +18,16 @@ public class LoginController {
 	
 	@Autowired
 	UserRepository repo;
-	
+	@CrossOrigin(origins="https://8081-bafdabebdefeddaffcbacabafcefcfcbc.examlyiopb.examly.io") //"http://localhost:8081"
 	@PostMapping(value="/login")
 	public boolean checkUser(@RequestBody LoginModel data)
 	{
+		System.out.println("inside checkUSer");
 		String email=data.getEmail();
 		String password=data.getPassword();
+		System.out.println(email);
 		UserModel curr_user=repo.getUser(email,password);
-		if(!(curr_user.equals(null)))
+		if(!(curr_user==null))
 		{
 			//System.out.println("Login Successful");
 			curr_user.setActive(1);
--- spring/src/main/java/com/example/controller/UserController.java
+++ spring/src/main/java/com/example/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,20 +12,22 @@ import com.example.model.UserModel;
 import com.example.repo.UserRepository;
 
 @RestController
-
+@CrossOrigin(origins="https://8081-bafdabebdefeddaffcbacabafcefcfcbc.examlyiopb.examly.io")
 public class UserController {
 
 	@Autowired
 	UserRepository repo;
 	
 	@PostMapping(value="/addUser")
-	public void userSave(@RequestBody UserModel data)
+	public boolean userSave(@RequestBody UserModel data)
 	{
-		if((repo.checkUserExists(data.getEmail()))==null)
+		if((repo.checkUserExists(data.getEmail()))==null){
 			repo.save(data);
+			return true;
+		}
 		else
 			System.out.println("User already exists");
-		return ;
+		return false;
 	}
 	
 	public List<UserModel> getUsers()
